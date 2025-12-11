6bf16e109c0bd38971768d8e20c0bba0adcbfeb3
João Viana <57032457+vollous@users.noreply.github.com>
2025-12-11 18:04:13 +0000

    Clean up code

--- include/BSMPT/baryo_fhck/difeq_domain_wall.h
+++ include/BSMPT/baryo_fhck/difeq_domain_wall.h
@@ -70,32 +70,25 @@ struct Difeq_DomainWall : Difeq
     }
     else
     {
+      const double dz = z[k] - z[k - 1];
       std::vector<double> point; // middle point (phi(k) + phi(k-1)) / 2
       for (size_t j = 0; j < dim; j++)
         point.push_back((y[dim + j][k] + y[dim + j][k - 1]) / 2);
-      std::vector<double> dv                   = dV(point);
-      std::vector<std::vector<double>> hessian = Hessian(point);
+      const std::vector<double> dv                   = dV(point);
+      const std::vector<std::vector<double>> hessian = Hessian(point);
 
       // 0 <= j < dim -> phi'(z)
       for (size_t j = 0; j < dim; j++)
       {
         for (size_t n = 0; n < dim; n++)
         {
-          // std::cout << "->1\n";
-          s[j][0 * dim + indexv[n]] = -Delta(j, n); // 1
-          // std::cout << "->3\n";
-          s[j][1 * dim + indexv[n]] =
-              -1. / 2. * (z[k] - z[k - 1]) * hessian[j][n]; // 3
-          //  std::cout << "->5\n";
-          s[j][2 * dim + indexv[n]] = Delta(j, n); // 5
-                                                   //  std::cout << "->7\n";
-          s[j][3 * dim + indexv[n]] =
-              -1. / 2. * (z[k] - z[k - 1]) * hessian[j][n];
-          // 7
+          s[j][0 * dim + indexv[n]] = -Delta(j, n);                  // 1
+          s[j][1 * dim + indexv[n]] = -1. / 2. * dz * hessian[j][n]; // 3
+          s[j][2 * dim + indexv[n]] = Delta(j, n);                   // 5
+          s[j][3 * dim + indexv[n]] = -1. / 2. * dz * hessian[j][n]; // 7
         }
-        // std::cout << "->E phi \n";
         //  Equations for E(k,k-1)
-        s[j][jsf] = y[j][k] - y[j][k - 1] - (z[k] - z[k - 1]) * dv[j];
+        s[j][jsf] = y[j][k] - y[j][k - 1] - dz * dv[j];
       }
 
       // dim <= j < 2 * dim -> phi(z)
@@ -103,21 +96,14 @@ struct Difeq_DomainWall : Difeq
       {
         for (size_t n = 0; n < dim; n++)
         {
-          // std::cout << "->2\n";
-          s[dim + j][0 * dim + indexv[n]] =
-              -(z[k] - z[k - 1]) / 2. * Delta(j, n); // 2
-                                                     //  std::cout << "->4\n";
-          s[dim + j][1 * dim + indexv[n]] = -Delta(j, n); // 4
-          // std::cout << "->6\n";
-          s[dim + j][2 * dim + indexv[n]] =
-              -(z[k] - z[k - 1]) / 2. * Delta(j, n); // 6
-          // std::cout << "->8\n";
-          s[dim + j][3 * dim + indexv[n]] = Delta(j, n); // 8
+          s[dim + j][0 * dim + indexv[n]] = -dz / 2. * Delta(j, n); // 2
+          s[dim + j][1 * dim + indexv[n]] = -Delta(j, n);           // 4
+          s[dim + j][2 * dim + indexv[n]] = -dz / 2. * Delta(j, n); // 6
+          s[dim + j][3 * dim + indexv[n]] = Delta(j, n);            // 8
         }
-        //  std::cout << "->E psi \n";
         //  Equations for E(k,k-1)
         s[dim + j][jsf] = y[dim + j][k] - y[dim + j][k - 1] -
-                          (z[k] - z[k - 1]) * (y[j][k] + y[j][k - 1]) / 2;
+                          dz * (y[j][k] + y[j][k - 1]) / 2;
       }
     }
   }
