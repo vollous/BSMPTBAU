c19a530e6d652b9629f83029e2411161f8b20331
João Viana <57032457+vollous@users.noreply.github.com>
2026-03-19 10:25:54 +0000

    Inscrease number of points per cycle, better but still not good

--- include/BSMPT/baryo_fhck/TransportEquations.h
+++ include/BSMPT/baryo_fhck/TransportEquations.h
@@ -199,7 +199,7 @@ public:
    * low precision calculation.
    *
    */
-  const double StepsPerCycleLow = 10;
+  const double StepsPerCycleLow = 100;
 
   /**
    * @brief When \f$ S = 0 \f$ the \f$ \mu = \mu_0 e^{-\lambda z}\f$, by taking
@@ -208,7 +208,7 @@ public:
    * high precision calculation.
    *
    */
-  const double StepsPerCycleHigh = 20;
+  const double StepsPerCycleHigh = 200;
 
   /**
    * @brief Used for quick calculations (should not be that relevant)
