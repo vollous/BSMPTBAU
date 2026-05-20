e0c5dab88c3f2d43e69f3d18164b0111bb44fc4c
João Viana <57032457+vollous@users.noreply.github.com>
2026-05-20 13:08:34 +0100

    Correct kink print profile

--- src/baryo_fhck/TransportModel.cpp
+++ src/baryo_fhck/TransportModel.cpp
@@ -106,8 +106,8 @@ void TransportModel::Initialize()
   {
     SetEtaInterface();
     std::ofstream PathFile("kink_data.tsv");
-    PathFile << Lw << "\t" << localCalcEta->top_theta_sym.value() << "\t"
-             << localCalcEta->bot_theta_sym.value() << "\n";
+    PathFile << Lw << "\t" << localCalcEta->top_theta_brk.value() << "\t"
+             << localCalcEta->top_theta_sym.value() << "\n";
     PathFile.close();
   }
 }
