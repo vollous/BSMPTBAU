06445cfeb95d0ed769c27d7e53a6d6bfa4023dfa
João Viana <57032457+vollous@users.noreply.github.com>
2026-05-25 20:37:02 +0100

    Improve coverage

--- src/minimizer/LibNLOPT/MinimizeNLOPT.cpp
+++ src/minimizer/LibNLOPT/MinimizeNLOPT.cpp
@@ -4,7 +4,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include <BSMPT/minimizer/LibNLOPT/MinimizeNLOPT.h>
-#include <BSMPT/minimizer/MinimizePlane.h>
 #include <BSMPT/models/ClassPotentialOrigin.h>
 #include <BSMPT/models/IncludeAllModels.h>
 #include <BSMPT/utility/utility.h>
@@ -73,70 +72,6 @@ NLOPTReturnType MinimizeUsingNLOPT(const Class_Potential_Origin &model,
         std::vector<double>(), 0, nlopt::result::FORCED_STOP, false);
     return res;
   }
-}
-
-double NLOPTVEffPlane(const std::vector<double> &x,
-                      std::vector<double> &grad,
-                      void *data)
-{
-  (void)grad;
-  auto params = *static_cast<PointerContainerMinPlane *>(data);
-  std::vector<double> vMinTilde(std::begin(x), std::end(x));
-  auto vev = TransformCoordinates(vMinTilde, params);
-  return params.modelPointer->VEff(params.modelPointer->MinimizeOrderVEV(vev),
-                                   params.Temp);
-}
-
-NLOPTReturnType
-MinimizePlaneUsingNLOPT(const struct PointerContainerMinPlane &params)
-{
-  auto dim = params.modelPointer->get_nVEV() - 1;
-  nlopt::opt opt(nlopt::GN_ORIG_DIRECT_L, static_cast<unsigned int>(dim));
-  std::vector<double> LowerBound(dim, -300), UpperBound(dim, 300);
-  for (std::size_t i{0}; i < params.modelPointer->get_nVEV(); ++i)
-  {
-    if (i < params.Index)
-    {
-      if (std::abs(params.modelPointer->get_vevTreeMin(i)) > UpperBound.at(i))
-      {
-        UpperBound.at(i) = 1.5 * params.modelPointer->get_vevTreeMin(i);
-      }
-      if (-std::abs(params.modelPointer->get_vevTreeMin(i)) < LowerBound.at(i))
-      {
-        LowerBound.at(i) =
-            -1.5 * std::abs(params.modelPointer->get_vevTreeMin(i));
-      }
-    }
-    else if (i > params.Index)
-    {
-      if (std::abs(params.modelPointer->get_vevTreeMin(i)) >
-          UpperBound.at(i - 1))
-      {
-        UpperBound.at(i - 1) = 1.5 * params.modelPointer->get_vevTreeMin(i);
-      }
-      if (-std::abs(params.modelPointer->get_vevTreeMin(i)) <
-          LowerBound.at(i - 1))
-      {
-        LowerBound.at(i - 1) =
-            -1.5 * std::abs(params.modelPointer->get_vevTreeMin(i));
-      }
-    }
-  }
-  opt.set_lower_bounds(LowerBound);
-  opt.set_upper_bounds(UpperBound);
-
-  std::vector<double> VEV(params.modelPointer->get_nVEV() - 1);
-  auto copy = params;
-  opt.set_min_objective(NLOPTVEffPlane, &copy);
-  opt.set_xtol_rel(1e-4);
-  double minf;
-  auto result  = opt.optimize(VEV, minf);
-  auto minimum = TransformCoordinates(VEV, params);
-  bool Success = (result == nlopt::SUCCESS) or
-                 (result == nlopt::FTOL_REACHED) or
-                 (result == nlopt::XTOL_REACHED);
-  NLOPTReturnType res(minimum, minf, result, Success);
-  return res;
 }
 
 NLOPTReturnType
